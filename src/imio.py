1b17fe8d26a904384577ae68da8ef46da2a3de47
mstryjek <73649466+mstryjek@users.noreply.github.com>
2022-04-13 19:00:27 +0200

    feat: Add config parser
    Also create skeleton files for image I/O and main.

---
+++ src/imio.py
@@ -0,0 +1,10 @@
+"""
+MODULE TODO
+Image I/O module. Contains class responsible for reading images from 
+camera/video stream as well as saving as separate files or to a video file.
+MS
+"""
+import cv2
+import numpy as np
+
+## TODO
